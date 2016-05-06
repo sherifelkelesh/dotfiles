01ea66cf49784f22d41a96c94fa8532af34b9a39
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2016-05-06 10:01:08 -0300

    Add backup task

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,10 +7,27 @@
 rm -rf ~/.backup
 
 mkdir -p ~/.backup
+mkdir -p ~/.backup/zsh
+mkdir -p ~/.backup/git
+mkdir -p ~/.backup/vim
+mkdir -p ~/.backup/ubuntu
 
+# ZSH
 cp ~/.zshrc ~/.backup/zsh
-cp ~/.aliases ~/.backup/zsh
-cp ~/.gitconfig ~/.backup
-cp ~/.vimrc ~/.backup
+cp ~/.custom/.aliases ~/.backup/zsh
+cp ~/.custom/.config ~/.backup/zsh
+cp ~/.custom/.functions ~/.backup/zsh
+
+# Git
+cp ~/.gitconfig ~/.backup/git
+cp ~/.gitignore_global ~/.backup/git
+cp ~/.gitmessage ~/.backup/git
+
+# Vim
+cp ~/.vimrc ~/.backup/vim
+
+# Sublime
 cp -R ~/.config/sublime-text-3/Packages/User ~/.backup
-cp -R ~/.config/autostart ~/.backup
+
+# Autostartup files
+cp -R ~/.config/autostart ~/.backup/ubuntu
