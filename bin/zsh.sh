74a152269dc1c653890ecb771c6325df53f436fd
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2016-07-03 00:14:30 -0300

    Change bin and lib structures

--- bin/zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ bash
-
-#
-# ZSH Installation
-# --------------------------------------------------------------------
-
-sudo apt-get install zsh -y
-chsh -s /usr/bin/zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
