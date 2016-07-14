2b5e81d12342b67e7bbbc24e56dbdac45334a613
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2016-07-14 11:49:57 -0300

    Move dependencies donwload to github.com/iagodahlem/setup

--- lib/makes.sh
+++ lib/makes.sh
@@ -6,10 +6,3 @@
 
 mkdir -p ~/dev/repos
 mkdir -p ~/dev/projects
-
-mkdir -p ~/.custom/themes
-wget https://raw.githubusercontent.com/dracula/zsh/master/dracula.zsh-theme -O ~/.custom/themes/dracula.zsh-theme
-
-mkdir -p ~/.softwares
-
-mkdir -p ~/.backup
